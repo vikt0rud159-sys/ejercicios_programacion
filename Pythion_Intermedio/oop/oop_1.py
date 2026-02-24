993d784461073792060c7d4f7879fc2fd042a4a5
Victor <vikt0rud159@gmail.com>
2026-02-24 14:02:33 -0600

    ordenamiento

--- Pythion_Intermedio/oop/oop_1.py
+++ Pythion_Intermedio/oop/oop_1.py
@@ -4,7 +4,7 @@ import math
 class Circle:
     def __init__(self, radius):
         self.radius = radius
-    
+
     def get_area(self):
         self.area = math.pi * (self.radius ** 2)
         return self.area
